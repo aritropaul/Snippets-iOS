031cf140122c2aee20285e61e08d69feb6f10470
notacoder69 <aritro.pau61@gmail.com>
2018-10-02 05:40:15 +0530

    Snippet 1

--- Snippets/TextButtonViewController.swift
+++ Snippets/TextButtonViewController.swift
@@ -12,10 +12,18 @@ class TextButtonViewController: UIViewController,UITextFieldDelegate {
 
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var textButton: UIButton!
+    var Text = ""
+    var Syms:[String] = []
     
-    func textFieldReturn(textField:UITextField)-> Bool{
+    func textFieldShouldReturn(_ textField: UITextField)-> Bool{
         self.view.endEditing(true)
         textField.resignFirstResponder()
+        textField.alpha = 0
+        textField.isUserInteractionEnabled = false
+        textButton.alpha = 1
+        textButton.isUserInteractionEnabled = true
+        Text = textField.text ?? ""
+        print(symptomFinder(Text: Text))
         return true
     }
     
@@ -54,6 +62,21 @@ class TextButtonViewController: UIViewController,UITextFieldDelegate {
         textField.alpha = 0
         textField.isUserInteractionEnabled = false
         textField.delegate = self
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: self.textField.frame.height))
+        textField.layer.cornerRadius = 15
+        textField.leftView = paddingView
+        textField.leftViewMode = UITextField.ViewMode.always
+        textField.layer.backgroundColor = UIColor(red: 234/255, green: 234/255, blue: 234/255, alpha: 1).cgColor
+    }
+    
+    func symptomFinder(Text:String)->[String]{
+        Syms.removeAll()
+        for sym in symptoms{
+            if (Text.contains(sym)){
+                Syms.append(sym)
+            }
+        }
+        return Syms
     }
     
     override func viewDidLoad() {
@@ -64,19 +87,6 @@ class TextButtonViewController: UIViewController,UITextFieldDelegate {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
-        textField.alpha = 0
-        textField.isUserInteractionEnabled = false
-        textButton.alpha = 1
-        textButton.isUserInteractionEnabled = true
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
